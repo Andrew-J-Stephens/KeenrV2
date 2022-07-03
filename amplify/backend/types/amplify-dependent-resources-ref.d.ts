de609a07b67e697d7ba67a04552118a762a40fdd
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-07-03 16:01:28 -0400

    played around with datastore

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
-        "keenrtodo": {
+        "keenr": {
             "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
