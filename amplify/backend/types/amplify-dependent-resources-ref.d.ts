612be1878fcae774bcb67c054ddc10c69dbdd7bd
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-07-04 16:27:37 -0400

    configured S3 bucket for photos

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -17,5 +17,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "keenrPosts": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
