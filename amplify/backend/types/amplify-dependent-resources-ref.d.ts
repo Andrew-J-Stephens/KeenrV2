6ea90de4026e23f19ce1ae265d492d83ff023070
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-07-20 19:51:12 -0400

    fixed auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,16 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "keenrv2c25570d7c25570d7": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    },
     "api": {
         "keenrv2": {
             "GraphQLAPIKeyOutput": "string",
